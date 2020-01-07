ebbeca1e4fe71375429fe599e73cb5ee06937b1c
Adam Mornes <adammornes@gmail.com>
2020-01-07 11:18:05 -0600

    fixed backwards logic

--- src/utils/path-integrity.js
+++ src/utils/path-integrity.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 export default async function(directory, cb) {
     const exists = await fs.pathExists(directory);
     if(!exists) {
-        if(!cb && typeof cb !== 'function') {
+        if(!cb || typeof cb !== 'function') {
             console.log(err);
             console.error('%s There was an issue copying project files. Please try again', chalk.red.bold('ERROR'));
             process.exit(1);
