e6fef5abe40603602716e52cbffd2e70d3bb3dc0
Adam Mornes <adammornes@gmail.com>
2020-02-05 15:13:34 -0600

    cleaned up help formatting & mapping

--- src/actions/help.js
+++ src/actions/help.js
@@ -1,26 +1,48 @@
-export default function() {
-    const help = `
-    Usage: xc-cli <command> [options]
-    
-    Options:
-      -v, --version   output the version number
-      -h, --help      output usage information
+const help = {
+    default: [
+        {
+            heading: 'Usage: xc-cli <command> [options]',
+            lines: []
+        },
+        {
+            heading: 'Options:',
+            lines: [
+                '-v, --version   output the version number',
+                '-h, --help      output usage information'
+            ]
+        },
+        {
+            heading: 'Commands:',
+            lines: [
+                'init            generate a new project in current directory',
+                'help [cmd]      display help for [cmd]'
+            ]
+        }
+    ],
+    init: [
+        {
+            heading: 'Usage: xc-cli init [options]',
+            lines: []
+        },
+        {
+            heading: 'Options:',
+            lines: [
+                '-s, --simple     skip prompts and generate with no frameworks selected',
+                '-g, --git        initialize project as a Git repository',
+                '-i, --install    automatically install Node dependencies'
+            ]
+        }
+    ]
+};
 
-    Commands:
-      init            generate a new project in current directory
-      help [cmd]      display help for [cmd]
-    `;
-    console.log(help);
-}
+const display = arr => {
+    arr.forEach(item => {
+        console.log(item.heading);
+        item.lines.forEach(line => console.log(` ${line}`));
+        console.log();
+    });
+};
 
-export function init() {
-    const help = `
-    Usage: xc-cli init [options]
+export default display.bind(this, help.default);
 
-    Options:
-      -s, --simple     skip prompts and generate with no frameworks selected
-      -g, --git        initialize project as a Git repository
-      -i, --install    automatically install Node dependencies
-    `;
-    console.log(help);
-}
+export const init = display.bind(this, help.init);
