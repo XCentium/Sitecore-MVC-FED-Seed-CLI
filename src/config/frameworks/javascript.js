fe5b3e0ed77b565fc8db87da3f0f0fc3633894f3
Adam Mornes <adammornes@gmail.com>
2020-02-05 17:04:43 -0600

    base js framework config

---
+++ src/config/frameworks/javascript.js
@@ -0,0 +1,19 @@
+const frameworks = [
+    {
+        title: 'None'
+    },
+    {
+        title: 'Vue',
+        template: 'vue',
+        dependencies: {
+            "vue": "^2.6.11"
+        }
+    }
+];
+
+const defaultFramework = 'None';
+
+export { 
+    frameworks,
+    defaultFramework
+}
