53260526a24f6c618c9d59ed9b997a6dbacb3962
Adam Mornes <adammornes@gmail.com>
2020-02-06 11:19:20 -0600

    more abstractions

--- src/actions/create.js
+++ src/actions/create.js
@@ -3,12 +3,8 @@ import Listr from 'listr';
 import paths from '../config/paths';
 import { frameworks as cssFrameworks } from '../config/frameworks/css';
 import prompts from './prompts';
-import copy from './copy';
-import git from './git';
+import tasks from './tasks';
 import checkPathIntegrity from '../utils/path-integrity';
-import { projectInstall } from 'pkg-install';
-
-import injectDependency from './inject-dependency';
 import parseTemplate from './parse-template';
 
 export default async function(args) {
@@ -23,34 +19,9 @@ export default async function(args) {
 
     const cssTemplate = await parseTemplate(cssFrameworks, options.cssFramework, 'css');
 
-    const tasks = new Listr([
-        {
-            title: 'Copy common project files',
-            task: () => copy(paths.templates.common, options.targetDirectory)
-        },
-        {
-            title: 'Copy CSS framework project files',
-            task: () => copy(cssTemplate.dir, options.targetDirectory),
-            skip: () => !cssTemplate.dir
-        },
-        {
-            title: 'Inject CSS framework dependencies',
-            task: () => injectDependency(cssTemplate.dependencies),
-            skip: () => !cssTemplate.dir
-        },
-        {
-            title: 'Initialize git',
-            task: () => git(options.targetDirectory),
-            enabled: () => options.git
-        },
-        {
-            title: 'Install dependencies',
-            task: () => projectInstall({ cwd: options.targetDirectory }),
-            skip: () => !options.runInstall ? 'Pass --install to automatically install dependencies': undefined
-        }
-    ]);
+    const queue = new Listr(tasks(options, { cssTemplate }));
+    await queue.run();
 
-    await tasks.run();
     console.log('%s Project ready', chalk.green.bold('DONE'));
     return true;
 }
--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,10 +1,34 @@
-import copyFiles from './copy-files';
+import paths from '../config/paths';
+import copy from './copy';
+import git from './git';
+import { projectInstall } from 'pkg-install';
+import injectDependency from './inject-dependency';
 
-export default function(options) {
+export default function(options, templates) {
     return [
         {
-            title: 'Copy CSS Framework files',
-            task: () => 
+            title: 'Copy common project files',
+            task: () => copy(paths.templates.common, options.targetDirectory)
+        },
+        {
+            title: 'Copy CSS framework project files',
+            task: () => copy(templates.cssTemplate.dir, options.targetDirectory),
+            skip: () => !templates.cssTemplate.dir
+        },
+        {
+            title: 'Inject CSS framework dependencies',
+            task: () => injectDependency(templates.cssTemplate.dependencies),
+            skip: () => !templates.cssTemplate.dir
+        },
+        {
+            title: 'Initialize git',
+            task: () => git(options.targetDirectory),
+            enabled: () => options.git
+        },
+        {
+            title: 'Install dependencies',
+            task: () => projectInstall({ cwd: options.targetDirectory }),
+            skip: () => !options.runInstall ? 'Pass --install to automatically install dependencies': undefined
         }
     ]
 }
