50d258abc38fc68f97bb3d3b700857409e8a5349
Adam Mornes <adammornes@gmail.com>
2020-02-06 11:22:14 -0600

    added javascript question to prompts

--- src/actions/prompts.js
+++ src/actions/prompts.js
@@ -2,12 +2,14 @@ import inquirer from 'inquirer';
 import multichoice from '../utils/inquirer-multichoice';
 import * as questions from '../config/questions';
 import { defaultFramework as defaultCssFramework } from '../config/frameworks/css';
+import { defaultFramework as defaultJavascriptFramework } from '../config/frameworks/javascript';
 
 export default async function(options) {
     if(options.skipPrompts) {
         return {
             ...options,
-            cssFramework: defaultCssFramework
+            cssFramework: defaultCssFramework,
+            javascriptFramework: defaultJavascriptFramework
         }
     }
 
@@ -15,6 +17,10 @@ export default async function(options) {
 
     if(!options.cssFramework) {
         questionQueue.push(multichoice(questions.css));
+    }
+
+    if(!options.javascriptFramework) {
+        questionQueue.push(multichoice(questions.javascript));
     }
 
     if(!options.git) {
